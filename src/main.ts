4098147a9d2732e1d71e8319f5b9f156db74461b
guqing <i@guqing.email>
2022-10-31 15:17:23 +0800

    refactor: theme schema color

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./assets/colors/white.css"
 import "./assets/colors/light.css"
 import "./assets/colors/dark.css"
-import "./assets/style.css"
 import "./assets/main.js"
+import "./styles/main.css";
 import '@purge-icons/generated'
