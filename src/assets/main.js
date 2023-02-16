6db88aed0851d32130f974249a2771ef835c8d76
guqing <i@guqing.email>
2023-02-16 16:52:07 +0800

    feat: mobile menu actions

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,3 +1,15 @@
+/**
+ * Sets up Justified Gallery.
+ */
+if (!!$.prototype.justifiedGallery) {
+  var options = {
+    rowHeight: 140,
+    margins: 4,
+    lastRow: "justify"
+  };
+  $(".article-gallery").justifiedGallery(options);
+}
+
 $(function () {
   handlePreviewImage();
 
