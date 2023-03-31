d68fae5cf268529ce1a09b2dc265bff84151ac2f
guqing <i@guqing.email>
2023-03-31 18:06:55 +0800

    feat: add moments template

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+import type { Alpine } from "alpinejs";
+
+export {};
+
+declare global {
+  interface Window {
+    Alpine: Alpine;
+    SearchWidget: any;
+  }
+}
